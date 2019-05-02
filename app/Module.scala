d273c915f822c5f84870d26aaec39522f13224b3
Stefano Tondo <stondo@gmail.com>
2019-05-02 10:43:07 +0200

    added custom Request and Error handlers

--- app/Module.scala
+++ app/Module.scala
@@ -2,17 +2,12 @@ import com.google.inject.AbstractModule
 import net.codingwell.scalaguice.ScalaModule
 import play.api.{Configuration, Environment}
 
-
 /**
   * Sets up custom components for Play.
   *
   * https://www.playframework.com/documentation/latest/ScalaDependencyInjection
   */
-class Module(environment: Environment, configuration: Configuration)
-  extends AbstractModule
-    with ScalaModule {
+class Module(environment: Environment, configuration: Configuration) extends AbstractModule with ScalaModule {
 
-  override def configure() = {
-
-  }
+  override def configure(): Unit = {}
 }
